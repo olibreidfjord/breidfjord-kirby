4c4ee746b4587f658a789482a6b7cba7ae2a284e
root <root@digitalocean-server-1>
2026-03-02 15:17:34 +0000

    Initial Kirby installation

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require __DIR__ . '/kirby/bootstrap.php';
+echo (new Kirby)->render();
---
+++ site/config/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'debug' => false,
+    'yaml.handler' => 'symfony',
+    'url' => 'https://kirby.olafurbreidfjord.com',
+    'panel.install' => false,
+];
