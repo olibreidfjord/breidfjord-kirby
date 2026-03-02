67cb6bc9abe2a603ac065f7cc0df5b5a5cb9398d
Olafur Breidfjord <olafurbreidfjord@Mac.powerhub>
2026-03-02 15:59:15 +0000

    Dynamic URL config for local/production environments

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,11 @@
 <?php
+
+$host = $_SERVER['HTTP_HOST'] ?? '';
+$url = str_contains($host, 'kirby.test') ? 'https://kirby.test' : 'https://kirby.olafurbreidfjord.com';
+
 return [
-    'debug' => false,
+    'debug' => str_contains($host, 'kirby.test'),
     'yaml.handler' => 'symfony',
-    'url' => 'https://kirby.olafurbreidfjord.com',
+    'url' => $url,
     'panel.install' => false,
 ];
